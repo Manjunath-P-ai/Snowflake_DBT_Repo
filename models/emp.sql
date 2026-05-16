0a706b40e473c89f0f65c4d9c0f97e8ef78f6e54
p11605324@gmail.com <p11605324@gmail.com>
2026-05-16 21:30:11 +0000

    Initializing new change

---
+++ models/emp.sql
@@ -0,0 +1,1 @@
+select * from db_ecom.details.emp_details
---
+++ models/empnew.sql
@@ -0,0 +1,5 @@
+select * from db_ecom.details.emp_details
+UNION ALL
+select * from db_ecom.details.emp_details
+UNION ALL
+select * from db_ecom.details.emp_details
